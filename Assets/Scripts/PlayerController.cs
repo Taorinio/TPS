a717c6ca401d9f3c5000d264d3bfa59dcd236750
Tenshimi Tao <sensentore@gmail.com>
2024-02-11 17:54:08 +0300

    Added Walking (but it has a bug)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float fallVelocity = 1f;
+    public float jumpForce = 6.5f;
+    public float speed = 1f;
+    CharacterController CharCont;
+    Vector3 moveVector;
+
+    void Start()
+    {
+        CharCont = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveVector = transform.forward * Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime + transform.right * Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime + Vector3.down * fallVelocity * Time.fixedDeltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && CharCont.isGrounded) {
+            fallVelocity = -jumpForce;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        fallVelocity += CharCont.isGrounded ? 0 : 9.81f * Time.fixedDeltaTime;
+        CharCont.Move(moveVector);
+    }
+}
