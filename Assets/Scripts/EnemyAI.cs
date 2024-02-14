25cb018a96da5adcdc5987eeb8af5fa393315bc5
Tenshimi Tao <sensentore@gmail.com>
2024-02-14 23:28:46 +0300

    Added AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+    public List<Transform> patrolPoints;
+    private Transform randomSelected;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (randomSelected == null) randomSelected = patrolPoints[Random.Range(0, patrolPoints.Count)];
+        if (_navMeshAgent.gameObject.transform.position != new Vector3(randomSelected.position.x, _navMeshAgent.gameObject.transform.position.y, randomSelected.position.z)) {
+            _navMeshAgent.destination = randomSelected.position;
+        }
+        else {
+            randomSelected = null;
+        }
+    }
+}
