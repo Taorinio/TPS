630531e66ba4af83617d2c11db7b2c91980b5706
Tenshimi Tao <sensentore@gmail.com>
2024-02-16 03:06:57 +0300

    Better AI, Refactoring and more...

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,7 +7,10 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
     public List<Transform> patrolPoints;
+    public CharacterController player;
     private Transform randomSelected;
+    private bool isPlayerNoticed;
+    public float fieldOfView = 60f;
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
@@ -16,12 +19,44 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (randomSelected == null) randomSelected = patrolPoints[Random.Range(0, patrolPoints.Count)];
-        if (_navMeshAgent.gameObject.transform.position != new Vector3(randomSelected.position.x, _navMeshAgent.gameObject.transform.position.y, randomSelected.position.z)) {
-            _navMeshAgent.destination = randomSelected.position;
+        Raysee();
+        Patrol();
+    }
+
+    void Patrol()
+    {
+        if (!isPlayerNoticed)
+        {
+            if (randomSelected == null) randomSelected = patrolPoints[Random.Range(0, patrolPoints.Count)];
+            if (_navMeshAgent.gameObject.transform.position != new Vector3(randomSelected.position.x, _navMeshAgent.gameObject.transform.position.y, randomSelected.position.z))
+            {
+                _navMeshAgent.destination = randomSelected.position;
+            }
+            else
+            {
+                randomSelected = null;
+            }
         }
-        else {
-            randomSelected = null;
+        else
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+    
+    void Raysee()
+    {
+        var direction = player.transform.position - transform.position;
+        isPlayerNoticed = false;
+        if (Vector3.Angle(transform.forward, direction) < fieldOfView)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    isPlayerNoticed = true;
+                }
+            }
         }
     }
 }
